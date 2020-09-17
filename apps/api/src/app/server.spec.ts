3280c0eade076e13ee103a0fae0edb3d7d50034e
Michael Lage <lage.michael@gmail.com>
2020-09-17 15:37:51 -0600

    unit-test server
    - sets up actual graphql api server in listen mode, runs a client query
      against it then evaluates results

---
+++ apps/api/src/app/server.spec.ts
@@ -0,0 +1,48 @@
+import { ApolloClient, InMemoryCache, gql, HttpLink } from '@apollo/client';
+import fetch from 'cross-fetch';
+
+import App from './server';
+import { mockPlant1, mockPlant2 } from './resolvers';
+
+describe('Server', () => {
+  const port = 1337;
+
+  let api:any; 
+
+  beforeAll(() => {
+    api = App.server.listen({ port });
+  })
+
+  it('should initiate a GraphQL server with our schema, queries and mutations', async() => {
+    const client = new ApolloClient({
+      link: new HttpLink({ uri: `http://localhost:${port}/graphql`, fetch }),
+      cache: new InMemoryCache()
+    });
+
+    const result = await client.query({
+      query: gql`
+        query getPlants {
+          plants {
+            name
+          }
+        }
+      `,
+    })
+
+    expect(result).toEqual({ 
+      data: { 
+        plants: [
+          {"__typename": "Plant", name: mockPlant1.name},
+          {"__typename": "Plant", name: mockPlant2.name}
+        ]
+      },
+      loading: false,
+      networkStatus: 7
+    });
+      
+  })
+
+  afterAll(() => {
+    api.close();
+  })
+})
