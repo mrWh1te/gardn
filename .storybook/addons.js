1661e07b51718a80a34d317c204d34a3650d99fa
Michael Lage <lage.michael@gmail.com>
2020-09-18 14:05:28 -0600

    new storybook-logo wip

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,6 +4,7 @@ import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
 
 const theme = create({
+  brandImage: 'https://raw.githubusercontent.com/mrWh1te/gardn/master/.storybook/storybook-logo.png'
 });
 
 addons.setConfig({
