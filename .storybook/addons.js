8a6d363a29f238b569de9b30e0eeb6c9a486dc8a
Michael Lage <lage.michael@gmail.com>
2020-09-18 13:56:27 -0600

    custom theme for storybook
    - applied globally WIP logo

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,1 +1,12 @@
 import '@storybook/addon-knobs/register';
+
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
+
+const theme = create({
+  brandTitle: 'gardn UI'
+});
+
+addons.setConfig({
+  theme
+});
