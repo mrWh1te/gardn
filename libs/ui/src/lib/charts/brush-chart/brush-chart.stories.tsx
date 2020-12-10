11e517b9702e50c484d57b8f579f1f6548a813da
Michael Lage <lage.michael@gmail.com>
2020-12-09 19:56:29 -0700

    Added AirBnb visx packages for UI lib BrushChart
    - copied the example code over, have the example code from their docs
      working inside storybook

---
+++ libs/ui/src/lib/charts/brush-chart/brush-chart.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { text, boolean } from '@storybook/addon-knobs';
+
+import BrushChart from './';
+
+export default { title: 'Charts/BrushChart' };
+
+export const example = () => (
+  <BrushChart width={400} height={300} />
+);
